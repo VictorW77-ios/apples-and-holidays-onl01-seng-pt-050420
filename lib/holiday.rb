53d4e0bac934e266d77f5b719d75366e5391f627
Victor Williams <victor.williams1030@gmail.com>
2020-05-15 22:20:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -3,12 +3,6 @@ require 'pry'
  # Write a method that returns the second supply for the Fourth of July
  
 def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_hash = {:winter => 
-  {:christmas => ["Lights", "Wreath"], :new_years => ["Party Hats"] },
-  :summer => {:fourth_of_july => ["Fireworks", "BBQ"] }, 
-  :fall => {:thanksgiving => ["Turkey"] }, 
-  :spring => {:memorial_day => ["BBQ"] } 
-  }
   holiday_hash[:summer][:fourth_of_july][1]
 end
 
