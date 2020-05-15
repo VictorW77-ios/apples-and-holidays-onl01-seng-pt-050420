b6be7bb3e194bb24e9aedf07a5db4ae70314cf31
Victor Williams <victor.williams1030@gmail.com>
2020-05-15 22:42:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  supples = []
+  supplies = []
   holiday_hash.each do |season, holiday| 
     supplies.push(holiday.values)
   end
