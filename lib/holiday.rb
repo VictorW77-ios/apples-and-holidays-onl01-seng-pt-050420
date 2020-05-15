3b49f3d620351689ca50331af9f124380523deb6
Victor Williams <victor.williams1030@gmail.com>
2020-05-15 23:13:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,6 +79,9 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.collect do |holiday, supply|
       if supply.include?("BBQ")
         holiday 
+      end
+    end
+  
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
